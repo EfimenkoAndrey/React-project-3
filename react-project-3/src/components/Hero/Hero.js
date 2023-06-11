0262c1d3d2c51716f0f35f3548bcd88bd758be0f
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2023-06-11 17:48:14 +0300

    feat: add section Hero

---
+++ react-project-3/src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hero.css';
+import heroMainPicture from '../../assets/images/Group.png';
+
+export default function Hero() {
+  return (
+    <section className='Hero'>
+      <img 
+      src = { heroMainPicture }
+      alt = 'Hero-img'
+      className = 'hero--img'
+      />
+      <h1 className='hero--title'>Online Experiences</h1>
+      <p className='hero--text-content'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+      </p>
+    </section>
+  )
+}
