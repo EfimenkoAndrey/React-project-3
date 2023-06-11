549267a10f93066a086e4a7da562899cf79e9b92
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2023-06-11 17:23:11 +0300

    feat: add Navbar

---
+++ react-project-3/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/images/logo-airbnb.png';
+
+export default function Navbar() {
+  return (
+    <nav className='Navbar'>
+      <img
+      src = { logo }
+      alt = 'Logo-airbnb'
+      className = 'navbar--logo'
+      />
+    </nav>
+  )
+}
